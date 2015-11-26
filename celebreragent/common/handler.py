91e06cff1410e9c734534ae02d379b10d0856d55
Alexey Galkin <agalkin@mirantis.com>
2015-11-26 20:08:10 +0300

    Some fixes for CelebrerHandler:  - Optimize work with services list  - Refactoring base64 encode process  - Add support '--omit' with generate reports  - Some refactoring run/stop coverage functions

--- celebreragent/common/handler.py
+++ celebreragent/common/handler.py
@@ -4,7 +4,6 @@ import shutil
 import tarfile
 import time
 import os
-import base64
 
 from . import utils
 
@@ -14,77 +13,80 @@ class CelebrerHandler(object):
     def __init__(self, agent):
         self.agent = agent
 
-    def start_coverage(self, context, service_name):
-        component, service = self.agent.get_service(service_name)
+    def start_coverage(self, context, services):
+        for service_name in services:
+            component, service = self.agent.get_service(service_name)
 
-        if service:
-            commands.getoutput("service %s stop" % service_name)
-            print "Service %s stoppped" % service_name
+            if service:
+                commands.getoutput("service %s stop" % service_name)
+                print "Service %s stoppped" % service_name
 
-            cmd_run = "%s run --source=%s --parallel-mode %s &" % (
-                self.agent.get_coverage_exec(), component, "%s %s" % (
-                    service.service_params['exec'],
-                    service.service_args
+                cmd_run = "%s run --source=%s --parallel-mode %s &" % (
+                    self.agent.get_coverage_exec(), component, "%s %s" % (
+                        service.service_params['exec'],
+                        service.service_args
+                    )
                 )
+
+                cov_path = '/tmp/coverage_%s' % component
+
+                if os.path.exists(cov_path):
+                    shutil.rmtree(cov_path)
+
+                os.mkdir(cov_path)
+                utils.pushd(cov_path)
+
+                print "RUN: %s" % cmd_run
+
+                os.system(cmd_run)
+
+                time.sleep(3)
+
+                if service_name not in commands.getoutput("ps aux"):
+                    commands.getoutput('service %s start' % service_name)
+
+                utils.popd()
+
+    def stop_coverage(self, context, services):
+        for service_name in services:
+            component, service = self.agent.get_service(service_name)
+
+            os.system(
+                'kill $(ps hf -C %s | grep "%s" | awk "{print \$1;exit}");' %
+                (self.agent.get_coverage_exec(), service)
             )
+
+            commands.getoutput('service %s start' % service)
+
+            print "Service %s started" % service
 
             cov_path = '/tmp/coverage_%s' % component
+            utils.combine(cov_path)
 
-            if os.path.exists(cov_path):
-                shutil.rmtree(cov_path)
+            if self.agent.is_primary():
+                combine_path = '/tmp/coverage-combine_%s' % component
+                if not os.path.exists(combine_path):
+                    os.mkdir(combine_path)
 
-            os.mkdir(cov_path)
-            utils.pushd(cov_path)
-
-            print "RUN: %s" % cmd_run
-
-            os.system(cmd_run)
-
-            time.sleep(3)
-
-            if service_name in commands.getoutput("ps aux"):
-                pass
+                shutil.copyfile(
+                    '%s/.coverage' % cov_path,
+                    '%s/.coverage.%s' % (
+                        combine_path,
+                        self.agent.get_instance_id()
+                    )
+                )
+                utils.combine(combine_path)
             else:
-                commands.getoutput('service %s start' % service_name)
-
-            utils.popd()
-
-    def stop_coverage(self, context, service_name, component_name):
-        os.system('kill $(ps hf -C %s | grep "%s" | '
-                  'awk "{print \$1;exit}");' % (self.agent.get_coverage_exec(),
-                                                service_name))
-
-        commands.getoutput('service %s start' % service_name)
-
-        print "Service %s started" % service_name
-
-        cov_path = '/tmp/coverage_%s' % component_name
-        utils.combine(cov_path)
-
-        if self.agent.is_primary():
-            combine_path = '/tmp/coverage-combine_%s' % component_name
-            if not os.path.exists(combine_path):
-                os.mkdir(combine_path)
-
-            shutil.copyfile(
-                '%s/.coverage' % cov_path,
-                '%s/.coverage.%s' % (
-                    combine_path,
-                    self.agent.get_instance_id()
-                )
-            )
-            utils.combine(combine_path)
-        else:
-            with open('%s/.coverage' % cov_path) as binary_report:
-                # Send coverage report to primary controller
-                self.agent.call_rpc(
-                    'collector', 'collect_coverage',
-                    component_name=component_name,
-                    binary_data=utils.prepare_data(
-                        binary_report.read(), 'compress'
-                    ),
-                    node_uuid=self.agent.get_instance_id()
-                )
+                with open('%s/.coverage' % cov_path) as binary_report:
+                    # Send coverage report to primary controller
+                    self.agent.call_rpc(
+                        'collector', 'collect_coverage',
+                        component_name=component,
+                        binary_data=utils.prepare_data(
+                            binary_report.read(), 'compress'
+                        ),
+                        node_uuid=self.agent.get_instance_id()
+                    )
 
     def collect_coverage(self, component_name, binary_data, node_uuid):
         combine_path = '/tmp/coverage-combine_%s' % component_name
@@ -108,8 +110,10 @@ class CelebrerHandler(object):
 
         utils.pushd(cov_path)
 
-        commands.getoutput('%s xml' % self.agent.get_coverage_exec())
-        commands.getoutput('%s html' % self.agent.get_coverage_exec())
+        commands.getoutput('%s xml --omit=*/openstack/*,*/tests/*' %
+                           self.agent.get_coverage_exec())
+        commands.getoutput('%s html --omit=*/openstack/*,*/tests/*' %
+                           self.agent.get_coverage_exec())
         commands.getoutput('%s report --omit=*/openstack/*,*/tests/* -m > '
                            'report_%s.txt' % (self.agent.get_coverage_exec(),
                                               component_name))
@@ -125,7 +129,7 @@ class CelebrerHandler(object):
             self.agent.call_rpc(
                 'reports', 'collect_report',
                 component_name=component_name,
-                binary_data=base64.b64encode(binary_report.read())
+                binary_data=binary_report.read().encode('base64')
             )
         utils.popd()
         shutil.rmtree(cov_path)
