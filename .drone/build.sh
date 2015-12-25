ed1a41b4b2cb03e0bf2103697fc67c8166ed3a9b
Alexey Galkin <agalkin@mirantis.com>
2015-12-25 11:00:08 +0300

    Remove 'dh-autoreconf' package

--- .drone/build.sh
+++ .drone/build.sh
@@ -5,7 +5,7 @@ export DEBEMAIL="agalkin@mirantis.com"
 export CELEBRER_VERSION=$(cat setup.cfg | grep version | awk {' print $3 '})
 
 apt-get -y update
-apt-get -y install build-essential autoconf automake autotools-dev dh-make debhelper devscripts fakeroot xutils lintian pbuilder python-setuptools dh-autoreconf python-pbr
+apt-get -y install build-essential autoconf automake autotools-dev dh-make debhelper devscripts fakeroot xutils lintian pbuilder python-setuptools python-pbr
 
 cd ..
 mv celebrer-agent celebrer-agent-$CELEBRER_VERSION
